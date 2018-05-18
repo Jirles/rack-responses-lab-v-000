010148a77e2bf7cfe07e3b8063aff4b16dc242b8
Emily Jirles <emily.jirles@gmail.com>
2018-05-18 21:06:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,14 @@ class Application
     
     time = Kernal.rand(0..24)
     
-    resp.write "Current time: #{time}" 
+    resp.write "Current time: #{time}"
+    
+    if time > 0 && time < 12
+      resp.write "Good morning!"
+    elsif time > 12 && time < 17 
+      resp.write "Good afternoon!"
+    else 
+      resp.write "Good evening!"
     resp.finish
   end 
   
