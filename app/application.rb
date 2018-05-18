39a05661df835be4081ba7663845976d4e4be747
Emily Jirles <emily.jirles@gmail.com>
2018-05-18 21:18:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,11 +11,9 @@ class Application
     resp.write "\n"
     
     if time >= 0 && time < 12
-      resp.write "Good morning!"
-    elsif time >= 12 && time < 17 
-      resp.write "Good afternoon!"
+      resp.write "Morning"
     else 
-      resp.write "Good evening!"
+      resp.write "Afternoon"
     end 
     resp.finish
   end 
