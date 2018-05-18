56650661db1955a742b1b429c9ec48e95c89cea1
Emily Jirles <emily.jirles@gmail.com>
2018-05-18 21:03:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,11 @@ class Application
   
   def call(env)
     
-    resp = Rack::Response.new 
-    resp.write 
+    resp = Rack::Response.new
+    
+    time = Array(0..24).rand
+    
+    resp.write "Current time: #{time}" 
     resp.finish
   end 
   
